dd4f5e8154d6282c2eb2726fdb36802db0654b62
ianfshirley <ianfshirley88@gmail.com>
2022-11-26 20:18:36 -0800

    CC5 all required tests passing

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -13,6 +13,9 @@ You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
 const toLastNames = people => {
   // Solution code here...
+  return people.map((person) => {
+    return person.firstName + ' ' + person.lastName;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -24,6 +27,9 @@ Write a function named addValues that, given an array of numbers as input, uses 
 
 const addValues = (arr) => {
   // Solution code here...
+  return arr.reduce((acc, currentVal) => {
+    return acc + currentVal;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -40,6 +46,9 @@ Write a function named addPurchases that, given an array of objects as input, us
 
 const addPurchases = (arr) => {
   // Solution code here...
+  return arr.reduce((acc, curr) => {
+    return acc + curr.purchasePrice;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -52,6 +61,7 @@ Note: You may not use the array's built-in length property.
 
 const countNumberOfElements = (arr) => {
   // Solution code here...
+  return arr.reduce(count => count + 1, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -112,6 +122,10 @@ let starWarsData = [{
 
 const returnNames = (arr) => {
   // Solution code here...
+  return arr.reduce((names, curr) => {
+    names.push(curr.name);
+    return names;
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -124,6 +138,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 
 const reversedString = (str) => {
   // Solution code here...
+  return str.split('').reduce((reverse, current) => current + reverse, '');
 };
 
 /* ------------------------------------------------------------------------------------------------
