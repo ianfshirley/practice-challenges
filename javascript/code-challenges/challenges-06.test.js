58c462c432bbcfb8b0155902f678152c33bca817
ianfshirley <ianfshirley88@gmail.com>
2022-11-26 20:55:31 -0800

    CC6 all required tests passing

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -24,7 +24,14 @@ Returns: ['dyoll', 'eimaj'];
 
 const getNames = (arr) => {
   // Solution code here...
+  // let nameArr = arr.reduce((names, person) => {
+  //   names.push(person.name);
+  //   return names;
+  // }, []);
+  // return nameArr.map(str => str.split('').reduce((reverse, current) => current + reverse, ''));
+  return arr.map(person => person.name.split('').reverse().join(''));
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -35,6 +42,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  return str + ' The end.';
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -52,6 +60,7 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
+  arr.push(arr[0]);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -71,6 +80,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
